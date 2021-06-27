841b8b95c6c9bcbdff6fe2752f7e35d70e7e6bfe
Jittat Fakcharoenphol <jittat@gmail.com>
2021-06-27 23:15:27 +0700

    updated from 61

---
+++ year-2562-slides/scripts/buildslides.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+
+from buildutils import build
+
+template = "slides.template"
+
+def main():
+    if len(sys.argv) < 2:
+        print "Usage: buildslides.py [filename.tex]"
+        quit()
+    fname = sys.argv[1]
+    if fname.endswith('.'):
+        fname = fname + 'tex'
+    build(fname, "slides.template", "%s.slides.tex")
+
+if __name__ == '__main__':
+    main()
